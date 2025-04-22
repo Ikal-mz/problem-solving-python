b9070b5e32389a7385ae141f87c118f1ce54d7f9
ikal-mz <mzulfikarb11@gmail.com>
2025-04-22 15:50:38 +0900

    mencari angka terbesar dari 3 inputan

---
+++ mencari-nilai-terbesar.py
@@ -0,0 +1,7 @@
+# mencari angka terbesar dari 3 angka yang di masukan
+a = int(input("Angka pertama : "))
+b = int(input("Angka kedua : "))
+c = int(input("Angka ketiga : "))
+
+terbesar = max(a,b,c)
+print(f"Angka terbesar dari 3 inputan adalah {terbesar}")
