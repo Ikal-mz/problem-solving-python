c31a47ed3a6d71355222167316bf8c11f2f59591
ikal-mz <mzulfikarb11@gmail.com>
2025-04-22 15:44:00 +0900

    firs commit

---
+++ menentukan-ganjil-genap.py
@@ -0,0 +1,7 @@
+# mencari bilangan ganjil dan genap
+angka= int(input("Masukan angka : "))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap")
+else:
+    print(f"{angka} adalah bilang ganjil")
